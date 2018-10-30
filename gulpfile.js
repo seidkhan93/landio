42e16d0761c654ff73145b4e09fa1a7400cf4e60
Nursultan Berkimbay <nursultanberkimbay@Nursultans-MacBook-Pro.local>
2018-10-30 19:49:34 +0600

    feat: Add npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
  
 sass.compiler = require('node-sass');
  
 gulp.task('scss', function () {
     return gulp.src('./src/all.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./style.css'));
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('scss:watch', function () {
     gulp.watch('./src/*.scss', ['scss']);
 });
 
-gulp.task('default', [ 'scss:watch' ]);
+gulp.task('default', [ 'scss', 'scss:watch' ]);
